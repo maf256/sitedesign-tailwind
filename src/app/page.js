d23cb80f299e0ab10867e7923cfb8168e983538d
Majid <maf256@gmail.com>
2025-08-02 12:12:02 +0330

    added components

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
+import About from "@/components/About";
+import Career from "@/components/Career";
+import Contact from "@/components/Contact";
+import Expertise from "@/components/Expertise";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Majid</h1>
-      goodbye moon
-    </div>
+    <main className="">
+      <Hero />
+      <About />
+      <Expertise />
+      <Career />
+      <Contact />
+      <Navbar />
+    </main>
   );
 }
+
+export const config = {
+  unstable_runtimeJS: false,
+};
